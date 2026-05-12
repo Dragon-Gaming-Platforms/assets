3bbe544143780308cb19726c18580bf8a07eeffb
Dragon-Gaming-Platforms <jaxonkhyer@gmail.com>
2026-05-12 10:11:12 -0500

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+importScripts("https://cdn.jsdelivr.net/npm/@mercuryworkshop/scramjet@2.0.0-alpha/dist/scramjet.all.js");
+
+const { ScramjetServiceWorker } = $scramjetLoadWorker();
+const sj = new ScramjetServiceWorker();
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(async () => {
+    await sj.loadConfig();
+    if (sj.route(event)) {
+      return sj.fetch(event);
+    }
+    return fetch(event.request);
+  });
+});
+
+self.addEventListener("install",  ()  => self.skipWaiting());
+self.addEventListener("activate", (e) => e.waitUntil(self.clients.claim()));
